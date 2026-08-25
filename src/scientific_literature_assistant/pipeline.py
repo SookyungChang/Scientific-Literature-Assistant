d32ad7cfd107e5d2b8130b4da3d09a31ef9ffd36
sweetguma <sooensible@gmail.com>
2026-08-25 14:27:07 +0200

    Chore: test lines

--- src/scientific_literature_assistant/pipeline.py
+++ src/scientific_literature_assistant/pipeline.py
@@ -2,6 +2,7 @@ from scientific_literature_assistant.config import PAPERS_DIR
 from scientific_literature_assistant.chunking import split_text_into_chunks
 from scientific_literature_assistant.ingestion import extract_text_from_pdf
 from scientific_literature_assistant.embeddings import create_embeddings
+from scientific_literature_assistant.retrieval import retrieve
 
 def process_document(pdf_path: str, chunk_size: int = 1000, chunk_overlap: int = 200) -> list[dict]:
     """
@@ -33,8 +34,32 @@ def process_document(pdf_path: str, chunk_size: int = 1000, chunk_overlap: int =
 
 if __name__ == "__main__":
     # Example usage
-    pdf_path = PAPERS_DIR / "Chang_Human.pdf" # Replace with your PDF file path
+    pdf_path = PAPERS_DIR / "Nayak_Lyanna.pdf" # Replace with your PDF file path
     chunks_with_embeddings = process_document(pdf_path, chunk_size=1000, chunk_overlap=200)
+
+    for chunk in chunks_with_embeddings:
+        print(f"Chunk {chunk['chunk_id']}", f"| Page {chunk['page_number']}")
+        print(chunk['text'])
+        print('-'*50)
+    # query = "Whare is the information about their simulation box?"
+
+    # results = retrieve(
+    #     query,
+    #     chunks_with_embeddings,
+    #     top_k=5
+    # )
     
-    print(f"Created {len(chunks_with_embeddings)} chunks from the document.")
-    print(len(chunks_with_embeddings[0]["embedding"]))  # Print the dimensionality of the first embedding for verification
+    # # print(f"Created {len(chunks_with_embeddings)} chunks from the document.")
+    # # print(len(chunks_with_embeddings[0]["embedding"]))  # Print the dimensionality of the first embedding for verification
+    # # print(chunks_with_embeddings[0])
+
+    # print("\n===== Retrieval Results =====")
+
+    # print("query:", query)
+
+    # for i, result in enumerate(results, start=1):
+    #     print(f"\n--- Result {i} ---")
+    #     print(f"Document: {result.get('document', pdf_path.name)}")
+    #     print(f"Page: {result['page_number']}")
+    #     print(f"Similarity: {result['similarity']:.4f}")
+    #     print(f"Text: {result['text'][:500]}")
