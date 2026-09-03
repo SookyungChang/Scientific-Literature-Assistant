bb3d66f5bd0286067522b9edc921045f9851daff
sweetguma <sooensible@gmail.com>
2026-09-03 20:26:03 +0200

    Feat: add function for converting pdf to json

--- src/scientific_literature_assistant/json_ext.py
+++ src/scientific_literature_assistant/json_ext.py
@@ -2,10 +2,15 @@ import json
 from docling.document_converter import DocumentConverter
 from scientific_literature_assistant.config import PAPERS_DIR, JSON_DIR
 
-converter = DocumentConverter()
-pdf_path = PAPERS_DIR / "Chang_Human.pdf"
-result = converter.convert(pdf_path)
-data = result.document.export_to_dict()
+def convert_pdf_to_json(pdf_path: str):
+    json_path = JSON_DIR / f"{pdf_path.name}.json"
+    converter = DocumentConverter()
+    result = converter.convert(pdf_path)
+    data = result.document.export_to_dict()
 
-with open(JSON_DIR/ "Chang_Human.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+    with open(json_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+if __name__ == "__main__":
+    pdf_name = "Chang_Human.pdf"
+    convert_pdf_to_json(PAPERS_DIR / pdf_name)
