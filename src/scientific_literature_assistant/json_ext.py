3478cade297e91f6639d92aa6b37a77b4f9dab2d
sweetguma <sooensible@gmail.com>
2026-09-03 21:13:06 +0200

    Feat: with this py all of the pdfs in the paper folder be converted to json

--- src/scientific_literature_assistant/json_ext.py
+++ src/scientific_literature_assistant/json_ext.py
@@ -1,16 +1,26 @@
 import json
+from pathlib import Path
 from docling.document_converter import DocumentConverter
 from scientific_literature_assistant.config import PAPERS_DIR, JSON_DIR
 
-def convert_pdf_to_json(pdf_path: str):
-    json_path = JSON_DIR / f"{pdf_path.name}.json"
-    converter = DocumentConverter()
-    result = converter.convert(pdf_path)
-    data = result.document.export_to_dict()
+converter = DocumentConverter()
 
-    with open(json_path, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+def convert_pdf_to_json():
+    for file in PAPERS_DIR.iterdir():
+            json_name = f"{file.name.split('.')[0]}.json"
+            if json_name not in [f.name for f in JSON_DIR.iterdir()]:
+                json_path = JSON_DIR / json_name
+                pdf_path = PAPERS_DIR / file.name
+                result = converter.convert(pdf_path)
+                data = result.document.export_to_dict()
+                with open(json_path, "w", encoding="utf-8") as f:
+                    json.dump(data, f, ensure_ascii=False, indent=2)
+                print(f"\n##### Converted {file.name} to {json_name} and saved to {json_path}.\n")
+            else:
+                print(f"\n##### JSON file for {file.name} already exists. Skipping conversion.\n")
+                continue
+    
 
 if __name__ == "__main__":
-    pdf_name = "Chang_Human.pdf"
-    convert_pdf_to_json(PAPERS_DIR / pdf_name)
+    convert_pdf_to_json()
+
