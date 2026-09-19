1062e5d21d5fbed6bd441eb355521cc4bef31e0f
sweetguma <sooensible@gmail.com>
2026-09-19 17:35:17 +0200

    Feat: add a function get_collection

--- src/sla/rag/chroma_store.py
+++ src/sla/rag/chroma_store.py
@@ -4,15 +4,32 @@ import chromadb
 from sla.config import Config
 config = Config()
 
-client = chromadb.PersistentClient(
-    path="./chroma_db"
-)
+def get_collection():
+    client = chromadb.PersistentClient(
+    path=config.DB_DIR
+    )
+    collection = client.get_or_create_collection(
+            name=config.COLLECTION_NAME,
+            configuration={
+                "hnsw": {
+                    "space": "cosine"
+                }
+            }
+        )
+    return collection
 
-collection = client.get_or_create_collection(
-    name="scientific_papers",
-    configuration={
-        "hnsw": {
-            "space": "cosine"
-        }
-    }
-)
+def collection_info(collection):
+    print("Number of chunks:", collection.count())
+
+    papers = collection.get(
+        include=["metadatas"]
+    )
+
+    documents = set(
+        metadata["document"]
+        for metadata in papers["metadatas"]
+    )
+
+    print("Documents in DB:")
+    for document in documents:
+        print("-", document)
